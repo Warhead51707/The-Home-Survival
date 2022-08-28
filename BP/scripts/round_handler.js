5be5c5638bdd7be5d43b0edde9dfd04e8460cd94
Chikorita Lover <cjgardels@gmail.com>
2022-08-28 18:57:56 -0500

    Add round progress bar
    - Progress bar that shows monsters or time remaining
    - Fixed game over message

--- BP/scripts/round_handler.js
+++ BP/scripts/round_handler.js
@@ -2,6 +2,11 @@ import { world, MolangVariableMap, EntityQueryOptions, BlockLocation } from "moj
 import { randomInt, getPlayers } from './utility.js'
 import { spawnPool } from "./spawn_pool.js"
 
+let progressQuery = new EntityQueryOptions()
+progressQuery.type = 'home:round_progress'
+
+let remainingMonsters = 0
+
 world.events.beforeChat.subscribe(data => {
     const message = data.message
     const dimension = world.getDimension(data.sender.dimension.id)
@@ -26,6 +31,11 @@ world.events.beforeChat.subscribe(data => {
 
 function startWave(dimension, round) {
     const totalMonsters = round * 3 + 3
+    remainingMonsters = totalMonsters
+
+    clearRoundProgress()
+
+    setRoundProgress(remainingMonsters, totalMonsters, true, "monsters")
 
     dimension.runCommand(`function expired`)
     dimension.runCommand(`title @a title §4Round ${round}`)
@@ -73,15 +83,7 @@ function startWave(dimension, round) {
         const alivePlayers = getPlayers(false)
 
         if ((alivePlayers <= 0 || alivePlayers === undefined) && !ended) {
-            dimension.runCommand(`tellraw @a {"rawtext":[{"text":"Game Over! You lasted ${round} rounds."}]}`)
-            dimension.runCommand(`function fail`)
-
-            world.events.tick.unsubscribe(spawnZombs)
-
-            ended = true
-            spawnEnd = true
-            roundEndA = true
-            roundEndB = true
+            endRound(false)
         }
 
         if (!roundEndA) {
@@ -90,7 +92,12 @@ function startWave(dimension, round) {
 
         if (roundEndA && !roundEndB) {
             endTicks++
+
+            setRoundProgress(endTicks, 600, endTicks === 1, "timer")
+
             if (endTicks === 600) {
+                clearRoundProgress()
+
                 roundEndB = true
             }
         }
@@ -159,20 +166,59 @@ function startWave(dimension, round) {
     }
 
     function checkZombs() {
+        setRoundProgress(remainingMonsters, totalMonsters, false, "monsters")
+
         if (spawnEnd) {
             let monsterQuery = new EntityQueryOptions()
             monsterQuery.families = ['monster']
+            const monsters = dimension.getEntities(monsterQuery)
 
-            if (Array.from(dimension.getEntities(monsterQuery)).length === 0) {
-                endRound()
+            if (Array.from(monsters).length === 0) {
+                endRound(true)
             }
         }
     }
 
-    function endRound() {
-        dimension.runCommand(`title @a title §eRound End`)
+    /**
+    * @remarks Ends the current round.
+    * @param {boolean} victory If true, the players won the round.
+    * @throws This function can throw errors.
+    */
+    function endRound(victory) {
+        clearRoundProgress()
 
         roundEndA = true
+
+        if (victory) {
+            dimension.runCommand("title @a title §eRound End")
+        } else {
+            dimension.runCommand(`tellraw @a {"rawtext":[{"text":"Game Over! You lasted ${round - 1} ${round === 2 ? "round" : "rounds"}."}]}`)
+            dimension.runCommand("function fail")
+
+            world.events.tick.unsubscribe(spawnZombs)
+
+            ended = true
+            spawnEnd = true
+            roundEndB = true
+        }
+    }
+
+    /**
+    * @remarks Kills all round progress entities that don't share the specified name tag.
+    * @param {string} nameTag Optional name tag that will be checked.
+    * @returns {boolean} Returns true if any entities were killed.
+    */
+    function clearRoundProgress(nameTag = "") {
+        let result = false
+
+        for (let entity of dimension.getEntities(progressQuery)) {
+            if (entity.nameTag !== nameTag || nameTag === "") {
+                entity.triggerEvent("home:instant_despawn")
+                result = true
+            }
+        }
+
+        return result
     }
 
     /**
@@ -205,5 +251,49 @@ function startWave(dimension, round) {
         return spawnGroup[randomInt(0, spawnGroup.length - 1)]
     }
 
+    /**
+    * @remarks Summons a new round progress entity, then sets its health and name tag accordingly.
+    * @param {number} value The number of monsters or time remaining.
+    * @param {number} max The maximum number of monsters or time that may remain.
+    * @param {boolean} override If true, a new entity will be summoned and older entities will be despawned regardless of other conditions.
+    * @param {string} mode Optional parameter that sets hardcoded properties for the round progress.
+    * @throws This function can throw errors.
+    */
+    function setRoundProgress(value, max, override = false, mode = "monsters") {
+        let progressTitle = "Round " + (round + (mode === "timer" ? 1 : 0))
+
+        if (value < 3 && mode === "monsters") {
+            progressTitle = "Monsters Remaining: " + value
+        }
+
+        if (override) {
+            clearRoundProgress()
+
+            dimension.runCommand(`summon home:round_progress "${progressTitle}" -168 -48 37`)
+        } else {
+            const newProgress = clearRoundProgress(progressTitle)
+
+            if (newProgress) {
+                dimension.runCommand(`summon home:round_progress "${progressTitle}" -168 -48 37`)
+            }
+        }
+
+        for (let progress of dimension.getEntities(progressQuery)) {
+            progress.getComponent("minecraft:health").setCurrent(value / max * 500)
+
+            if (progress.getComponent("minecraft:health").current <= 0) {
+                progress.triggerEvent("home:instant_despawn")
+            }
+        }
+    }
+
     world.events.tick.subscribe(() => spawnZombs())
 }
+
+world.events.entityHurt.subscribe(entityHurt => {
+    const entity = entityHurt.hurtEntity
+
+    if (entity.getComponent("minecraft:health").current <= 0 && entity.hasTag("monster")) {
+        remainingMonsters--
+    }
+})
